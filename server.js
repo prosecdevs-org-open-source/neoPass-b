ae542b4d8eedb15ab7eb4b58a059b6a72eeb8eed
vikas kumawat <vikaskumawat012356@gmail.com>
2024-03-26 09:43:43 +0530

    config env

--- server.js
+++ server.js
@@ -1,6 +1,16 @@
 const app = require('./app');
-const env = require('dotenv').config();
-
+const path = require('path');
+const envPath = path.resolve(__dirname, 'configs/.env');
+console.log(envPath);
+//const env = require('dotenv').config({ path: envPath});
+const dotenv = require('dotenv');
+try {
+    dotenv.config({ path: envPath });
+    console.log('Environment variables loaded successfully.');
+} catch (error) {
+    console.error('Error loading environment variables:', error);
+}
+console.log(process.env.port);
 // Rest of your code goes here
 
 port= process.env.port || 3000;
